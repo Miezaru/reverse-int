f1eb4e0d234ce10ae9d3832fbe8585f786d00ce6
Artem Usatyi <badlywaves@gmail.com>
2020-10-03 11:40:26 +0300

    init: solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = String(n);
+    let arr = [];
+    for (let i = 0; i < n.length; i++) {
+        if (n[i] === "-") continue;
+        arr.push(n[i]);
+    }
+    arr = Number(arr.reverse().join(""));
+    return arr;
+};
